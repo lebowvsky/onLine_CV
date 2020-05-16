9d05fba51d55ab70ce9a189c3e57587514f57438
Brice Le Gallo <lebowvsky@gmail.com>
2020-05-16 18:34:27 +0200

    Anim-title Done

--- src/components/TitleSpeciality.jsx
+++ src/components/TitleSpeciality.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
-import { useSpring, useTrail, animated, config } from 'react-spring';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faNodeJs, faJs } from '@fortawesome/free-brands-svg-icons';
-
+import React from "react";
+import { useSpring, useTrail, animated, config } from "react-spring";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faNodeJs, faJs } from "@fortawesome/free-brands-svg-icons";
 
 const TitleSpeciality = () => {
   const fontIcons = [
     <FontAwesomeIcon className="icons javascript" icon={faJs} />,
     <FontAwesomeIcon className="icons react" icon={faReact} />,
-    <FontAwesomeIcon className="icons node" icon={faNodeJs} />
+    <FontAwesomeIcon className="icons node" icon={faNodeJs} />,
   ];
   const iconsStyle = useTrail(fontIcons.length, {
-    transform: 'translateY(0)',
-    from: { transform: 'translateY(-60vh)' },
+    transform: "translateY(0)",
+    opacity: 1,
+    from: { transform: "translateY(-5vh)", opacity: 0 },
     delay: 1300,
-    config: config.wobbly
+    config: config.wobbly,
   });
-  const titleStyle = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 900, config: config.slow });
+  const titleStyle = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    delay: 900,
+    config: config.slow,
+  });
   return (
-    <div id="title-speciality" >
+    <div id="title-speciality">
       <animated.h2 style={titleStyle}>
-        Developpeur web & mobile<br/>
+        Developpeur web & mobile
+        <br />
         Javascript - React / Node.js
       </animated.h2>
       <div className="icons-container">
-        {
-          iconsStyle.map(({...rest}, index) => (
-            <animated.div style={{...rest}}>{fontIcons[index]}</animated.div>
-          ))
-        }
+        {iconsStyle.map(({ ...rest }, index) => (
+          <animated.div style={{ ...rest }}>{fontIcons[index]}</animated.div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default TitleSpeciality;
